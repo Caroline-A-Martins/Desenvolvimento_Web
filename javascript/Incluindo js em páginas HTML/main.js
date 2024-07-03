82f8ddffc56a10b63902fdd0e7e79c009a3572e8
Caroline-A-Martins <business.carolmartins@gmail.com>
2024-07-03 15:18:48 -0300

    Comentários

--- javascript/Incluindo js em páginas HTML/main.js
+++ javascript/Incluindo js em páginas HTML/main.js
@@ -1,4 +1,6 @@
-alert('Seja bem vindo!')
+//alert('Seja bem vindo!')
 
-
+/*
+    seleciona um elemento no DOM atualizar o valor desse elemento com uma string
+*/
 document.getElementById('nome').value = 'Oi'
